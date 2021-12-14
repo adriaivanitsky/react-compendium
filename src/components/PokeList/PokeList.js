908ea50fbf35b40db26a49021f729af4c9445704
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-12-13 17:29:02 -0800

    got list of pokemon to render on the page

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -3,9 +3,10 @@ import React from 'react';
 export default function PokeList({ pokemonList }) {
   return (
     <div>
-      {pokemonList.map((pokemon) => {
-        <p>{pokemon.pokemon}</p>;
-      })}
+      {pokemonList.map((pokemon) => (
+        <p key={pokemon._id}>{pokemon.pokemon}</p>
+      ))}
+      ;
     </div>
   );
 }
