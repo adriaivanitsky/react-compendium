4b8285bb6e3d0e15b7854db7fc99fcde21ed46fa
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-12-30 18:33:59 -0800

    added list of 5 attributes of each character

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -6,6 +6,11 @@ export default function PokeList({ pokemonList }) {
       {pokemonList.map((pokemon) => (
         <div key={pokemon._id}>
           <p>{pokemon.pokemon}</p>
+          <p>height: {pokemon.height}m</p>
+          <p>weight: {pokemon.weight}kg</p>
+          <p>shape: {pokemon.shape}</p>
+          <p>type 1: {pokemon.type_1}</p>
+          <p>type 2: {pokemon.type_2}</p>
           <img src={pokemon.url_image}></img>
         </div>
       ))}
