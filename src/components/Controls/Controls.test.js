81d8dc94ed808a88932ee03b22c0abfd5f7c6ebd
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-13 23:16:17 -0800

    update snapshot test

--- src/components/Controls/Controls.test.js
+++ src/components/Controls/Controls.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import Controls from './Controls.js';
 
-test('should render a ', async () => {
-  const container = render(<Controls />);
+test('should render a search bar ', async () => {
+  const container = render(<Controls types={['bug', 'ice']} />);
   await screen.findByPlaceholderText('search pokemon');
   expect(container).toMatchSnapshot();
 });
